fd13c9274339362087e735e4f76cdb1e9d3e6b77
Mateus Barros <mateusverissimo94@gmail.com>
2024-09-19 23:19:16 -0300

    subindo o pacote

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # package_name
 
-Description. 
+Description.
 The package package_name is used to:
-	- 
-	-
+Processing: - Histogram matching - Strutural similarity - Resize image
+Utils: - Read image - Save image - Plot image - Plot result - Plot histogram
 
 ## Installation
 
@@ -13,15 +13,10 @@ Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package
 pip install package_name
 ```
 
-## Usage
-
-```python
-from package_name.module1_name import file1_name
-file1_name.my_function()
-```
-
 ## Author
-My_name
+
+Mateus Barros
 
 ## License
+
 [MIT](https://choosealicense.com/licenses/mit/)
--- setup.py
+++ setup.py
@@ -7,15 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="image_processing",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Mateus",
+    description="image processing package using skimage",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/mateusverissimo94/package-template",
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires='>=3.5',
 )
